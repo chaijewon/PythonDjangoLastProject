cffa465c4b70a81f27f88d90e809dd741c38038c
chaijewon <vcandjava@nate.com>
2025-05-01 16:10:00 +0900

    Initial commit

---
+++ web/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from web import views
+from web import foodViews
+from web import recipeViews
+
+urlpatterns=[
+   path('',views.main_page),
+   path('food/list_vue/',foodViews.foodListData),
+   path('food/detail_vue/',foodViews.foodDetailData)
+]
